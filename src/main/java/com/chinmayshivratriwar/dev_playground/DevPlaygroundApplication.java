63748d98116e635d69fbf3ac7844adb1ad482fd2
Chinmay Shivratriwar <shivratriwarchinmay@gmail.com>
2026-01-16 22:27:48 +0530

    [CNS] - Initial Commit

---
+++ src/main/java/com/chinmayshivratriwar/dev_playground/DevPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.chinmayshivratriwar.dev_playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevPlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chinmayshivratriwar/dev_playground/DevPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chinmayshivratriwar.dev_playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
