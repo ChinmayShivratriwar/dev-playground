9a03bb23ef5bf69e479a8b4ffb7b5bc9e776eb9d
Chinmay Shivratriwar <shivratriwarchinmay@gmail.com>
2026-02-18 09:34:14 +0530

    [CNS] - CSES - OA

--- src/main/java/com/chinmayshivratriwar/dev_playground/cses/WeirdAlgorithm.java
+++ src/main/java/com/chinmayshivratriwar/dev_playground/cses/WeirdAlgorithm.java
@@ -1,20 +1,23 @@
 package com.chinmayshivratriwar.dev_playground.cses;
 
+import java.util.Scanner;
+
 public class WeirdAlgorithm {
     public static void main(String[] args) {
-        algorithm(3);
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        algorithm(n);
     }
 
-    private static void algorithm(int n) {
-        System.out.print(n + " ");
-        if(n == 1){
-            return;
+    private static void algorithm(long n) {
+        while(n != 1){
+            System.out.print(n + " ");
+            if(n%2 == 0){
+                n = n/2;
+            }else{
+                n = (n*3) +1;
+            }
         }
-        if(n%2 == 0){
-            algorithm(n/2);
-        }else{
-            algorithm((n*3) +1);
-        }
-
+        System.out.print(1);
     }
 }
